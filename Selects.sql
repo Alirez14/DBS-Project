c486e418430810782a2e0ad9bc3c821a3d11dbc4
Alirez14 <alireza_esi14@yahoo.com>
2018-11-22 13:05:43 +0100

    Project Online

---
+++ Selects.sql
@@ -0,0 +1,9 @@
+use Unternehm;
+
+select * from Abteilung;
+select * from Bestellung;
+select * from Kunde;
+select * from Angestellte;
+select * from Niederlassung;
+select * from Projekt;
+select * from Pro_Ang;
